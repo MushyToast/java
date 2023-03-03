fbd2bc27b214459237831d886f66fbb30c2684d4
Nolan <102072394+MushyToast@users.noreply.github.com>
2023-03-03 20:50:24 +0000

    e

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class App {
+    public static String input(String args) {
+        System.out.println(args);
+        Scanner inp = new Scanner(System.in);
+        return inp.nextLine();
+        };
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
-        
+        String[] string = {"one", "two", "three", "four", "five"};
+
+        for (int i = 0; i <= string.length-1; i++){
+            System.out.println(string[i]);
+        };
+        System.out.println(input("Enter name plz uwu"));
     }
 }
---
+++ src/main/java/com/mycompany/app/Loop.java
@@ -0,0 +1,10 @@
+class Loop {
+    public static void main(String[] args) {
+        String[] arr = {"Hello", "My", "Name", "Is", "Quandale", "Dinglenut", "XXV"};
+
+        for (int i = 0; i <= arr.length-1; i++){
+            arr[i];
+        }
+
+    }
+}
